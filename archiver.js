0b6102d1530921dcf4f65b18698210f9d579af49
chris-pie <amly.traveler@gmail.com>
2017-09-08 08:15:54 +0200

    Works, but with no error catching whatsoever.
    Also it's ugly but I'm not gonna work on it too much anyway.

--- archiver.js
+++ archiver.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Archives the messages
 // @author       Chris Pie
-// @match        https://www.tumblr.com/dashboard
+// @match        https://www.tumblr.com/message_archiver
 // @grant        GM_xmlhttpRequest
 // @grant        GM_log
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
@@ -13,14 +13,33 @@
 
 (function() {
     'use strict';
+    function timeConverter(UNIX_timestamp){
+        var a = new Date(UNIX_timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours() < 10 ? '0' + a.getHours() : a.getHours();
+        var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+        var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+    }
+    $("head").text("");
+    $("body").text("").append('<p id="counter"></p>');
+    var tumblog_mine = prompt("Enter YOUR blog name (without .tumblr.com)");
+    var tumblog_other = prompt("Enter THE OTHER BLOG'S name (without .tumblr.com)");
+
+    debugger;
     var convo_id ="";
-    var messages;
+    var messages_list = [];
     var done_gathering_messages = false;
     function get_messages(convo_id)
     {
         var waiting = false;
-        var url = "/svc/conversations/messages?conversation_id=446786&participant=kreuz-unlimited.tumblr.com";
+        var url = "/svc/conversations/messages?conversation_id="+convo_id;
         var repeater;
+        var message_counter = 0;
         function get_messages_http()
         {
             if(waiting) return;
@@ -39,19 +58,37 @@
                 {
                     var parsed_response = JSON.parse(response.responseText);
                     var messages = parsed_response.response.messages;
-                    waiting = false;
+                    for (var i = messages.data.length - 1; i>=0;i--)
+                    {
+                        var currmessage;
+                        if(messages.data[i].type === "TEXT")
+                        {
+                            // messages_list.unshift(timeConverter(messages.data.ts) + ' ' + messages.data.participant + ': ' + messages.data.message);
+                            currmessage = timeConverter(messages.data[i].ts.substr(0,10)) + ' ' + messages.data[i].participant + ': ' + messages.data[i].message;
+                        }
+                        else if (messages.data[i].type === "POSTREF")
+                        {
+                            //messages_list.unshift(timeConverter(messages.data.ts) + ' ' + messages.data.participant + ': ' + messages.data.post.post_url);
+                            currmessage = timeConverter(messages.data[i].ts.substr(0,10)) + ' ' + messages.data[i].participant + ': ' + messages.data[i].post.post_url;
+
+                        }
+                        message_counter++;
+                        $("#counter").text("Gathered " + message_counter + " messages.");
+                        $("#counter").after(currmessage + "<br/>");
+                    }
                     if (parsed_response.response.messages._links === undefined)
                     {
                         clearInterval(repeater);
                         waiting = false;
-                        done_gathering_messages = true;
+                        $("#counter").text($("#counter").text() + " Archiving complete. Press ctrl+a to select all, then ctrl+c to copy. Paste to notepad and save.");
                         return;
                     }
                     else url = parsed_response.response.messages._links.next.href;
+                    waiting = false;
                 }
             });
         }
-        repeater = setInterval(get_messages_http, 200);
+        repeater = setInterval(get_messages_http, 20);
     }
     function begin_archiving(tumblog_this, tumblog_remote)
     {
@@ -90,7 +127,6 @@
 
                         }
                     }
-                    waiting = false;
                     if (parsed_response.response._links === undefined)
                     {
                         convo_id = "error";
@@ -99,12 +135,13 @@
                         return;
                     }
                     else url = parsed_response.response._links.next.href;
+                    waiting = false;
                 }
             });
         }
         repeater = setInterval(get_conversations, 200);
     }
-    begin_archiving("kreuz-unlimited", "--invalidurl--");
+    begin_archiving(tumblog_mine, tumblog_other);
     var repeater;
     repeater = setInterval(function() {
         if (convo_id !== "")
